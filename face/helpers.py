ad53ec55115ed45e10b7254fd5e6c4a03b9d004c
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-02-11 17:46:33 -0800

    cleanup a bunch of help sketchwork

--- face/helpers.py
+++ face/helpers.py
@@ -1,131 +1,157 @@
 
 import os
+import sys
 import textwrap
+
+from boltons.iterutils import unique
 
 from face.parser import ERROR, Flag
 
-class AutoHelpBuilder(object):
+
+class HelpHandler(object):
     default_context = {
         'usage_label': 'Usage:',
         'subcmd_section_heading': 'Subcommands: ',
         'flags_section_heading': 'Flags: ',
-        'section_break': '\n\n',
-        'group_break': '\n',
+        'posargs_section_heading': 'Positional arguments:',
+        'section_break': '\n',
+        'group_break': '',
         'subcmd_example': 'subcommand',
+        'min_doc_width': 40,
+        'doc_separator': ' ',
+        'section_indent': '  '
     }
 
-    def __init__(self, cmd, ctx=None):
-        self.cmd = cmd
-        new_ctx = dict(self.default_context)
-        if ctx:
-            new_ctx.update(ctx)
-        self.ctx = new_ctx
+    def __init__(self, flag=('--help', '-h'), func=None, **kwargs):
+        ctx = {}
+        for key, val in self.default_context.items():
+            ctx[key] = kwargs.pop(key, val)
+        if kwargs:
+            raise TypeError('unexpected keyword arguments: %r' % list(kwargs.keys()))
+        self.ctx = ctx
+        self.func = func if func is not None else self.default_help_func
+        if not callable(self.func):
+            raise TypeError('expected func to be callable, not %r' % func)
 
-    def get_text(self):
-        # get usage string
-        lines = [self.get_usage_line()]
-        return '\n'.join(lines)
+    def default_help_func(self, cmd_, subcmds_):
+        print(self.get_help_text(cmd_.parser, subcmds=subcmds_))
+        sys.exit(0)
 
-    def get_usage_line(self):
-        cmd, ctx = self.cmd, self.ctx
+    def get_help_text(self, parser, subcmds=(), flags=None):
+        # TODO: filter by actually-used flags (note that help_flag and
+        # flagfile_flag are semi-built-in, thus used by all subcommands)
+        ctx = self.ctx
+        widths = self.get_widths(parser, subcmds)
+
+        ret = [self.get_usage_line(parser, subcmds=subcmds)]
+        append = ret.append
+
+        if subcmds:
+            parser = parser.subprs_map[subcmds]
+
+        append(ctx['group_break'])
+
+        if parser.doc:
+            append(parser.doc)
+            append(ctx['section_break'])
+
+        if parser.subprs_map:
+            append(ctx['subcmd_section_heading'])
+            append(ctx['group_break'])
+            for sub_name in unique([sp[0] for sp in parser.subprs_map if sp]):
+                subprs = parser.subprs_map[(sub_name,)]
+                append(ctx['section_indent'] + sub_name + '   ' + subprs.doc)
+            append(ctx['section_break'])
+
+        flags = parser.path_flag_map[()]
+        shown_flags = [f for f in flags.values() if not f.display.hidden]
+        if shown_flags:
+            append(ctx['flags_section_heading'])
+            append(ctx['group_break'])
+            for flag in unique(shown_flags):
+                label = flag.display.label
+                label_f = label.ljust(widths['max_flag_width'])
+                lhs = ctx['section_indent'] + label_f + ctx['doc_separator']
+                doc_parts = [] if not flag.doc else [flag.doc]
+                doc_parts.append(flag.display.post_doc)
+                full_doc = ' '.join(doc_parts)
+                if not full_doc:
+                    append(lhs)
+                    continue
+
+                wrapped_doc = textwrap.wrap(full_doc, widths['max_flag_doc_width'])
+                first_line = lhs + wrapped_doc[0]
+                append(first_line)
+                if len(wrapped_doc) == 1:
+                    continue
+
+                for line in wrapped_doc[1:]:
+                    append(' ' * widths['flag_doc_start'] + line)
+
+        return '\n'.join(ret)
+
+    def get_usage_line(self, parser, subcmds=()):
+        ctx = self.ctx
+        subcmds = tuple(subcmds or ())
         parts = [ctx['usage_label']] if ctx['usage_label'] else []
         append = parts.append
 
-        append(cmd.name)
+        append(' '.join((parser.name,) + subcmds))
 
-        if len(cmd.path_func_map) > 1:
+        # TODO: put () in subprs_map to handle some of this sorta thing
+        if not subcmds and parser.subprs_map:
+            append('subcommand')
+        elif subcmds and parser.subprs_map[subcmds].subprs_map:
             append('subcommand')
 
-        flags = cmd.parser.path_flag_map[()]
+        flags = parser.path_flag_map[subcmds]
         shown_flags = [f for f in flags.values() if f.display_name is not False]
 
         if shown_flags:
             append('[FLAGS]')
 
-        if cmd.parser.posargs:
-            if cmd.parser.posargs.display_full:
-                append(cmd.parser.posargs.display_full)
-            elif cmd.parser.posargs.min_count:
+        if parser.posargs:
+            if parser.posargs.display_full:
+                append(parser.posargs.display_full)
+            elif parser.posargs.min_count:
                 append('args ...')
             else:
                 append('[args ...]')
 
         return ' '.join(parts)
 
+    def get_widths(self, prs, subprs_path=(), max_width=None):
+        if max_width is None:
+            try:
+                max_width = int(os.environ['COLUMNS'])
+            except (KeyError, ValueError):
+                max_width = 80
+            max_width -= 2
 
-class SimpleUsageLineBuilder(object):
-    def __init__(self, cmd, ctx):
-        self.cmd = cmd
-        self.ctx = ctx
+        max_flag_width = 0
+        for flag in unique(prs.path_flag_map[subprs_path].values()):
+            cur_len = len(flag.display.label)
+            if cur_len > max_flag_width:
+                max_flag_width = cur_len
 
+        max_subcmd_width = 0
+        for subcmd_name in unique([path[0] for path in prs.subprs_map if path]):
+            cur_len = len(subcmd_name)
+            if cur_len > max_subcmd_width:
+                max_subcmd_width = cur_len
 
-class FullUsageLineBuilder(object):
-    def __init__(self, cmd, ctx):
-        self.cmd = cmd
-        self.ctx = ctx
+        min_doc_width = self.ctx['min_doc_width']
+        max_flag_doc_width = max_width - max_flag_width - len(self.ctx['doc_separator'])
 
-    def get_usage_line(self):
-        parts = []
-        subcmd_example = self.get_usage_subcmd_example()
-        if subcmd_example:
-            parts.append(subcmd_example)
+        flag_doc_start = len(self.ctx['section_indent'] + self.ctx['doc_separator']) + max_flag_width
+        subcmd_doc_start = len(self.ctx['section_indent'] + self.ctx['doc_separator']) + max_subcmd_width
 
-        posargs_example = self.get_usage_pos_arg_example()
-        if posargs_example:
-            parts.append(posargs_example)
-
-        return ' '.join(parts)
-
-    def _get_subcmd_depth(self):
-        return max([len(subcmd_path) for subcmd_path in self.cmd.path_func_map])
-
-    def get_usage_subcmd_example(self):
-        if not self._get_subcmd_depth():
-            return ''
-
-        if self.cmd.func:
-            return '[' + self.ctx['subcmd_example'] + ']'
-
-        return self.ctx['subcmd_example']
-        # try without angle brackets at first, see if it's
-        # sufficiently indicative of "required" return '<' +
-        # self.ctx['subcmd_example'] + '>'
-
-    def get_usage_pos_arg_example(self):
-        posargs = self.cmd.parser.posargs
-        if not posargs:
-            return ''
-
-        if posargs.display_full:
-            return posargs.display_full
-        if not posargs.display_name:
-            return ''  # a way of hiding that the command accepts pos args
-
-        display_name = posargs.display_name
-        min_count, max_count = posargs.min_count, posargs.max_count
-
-        if not min_count and not max_count:
-            return '[' + display_name + ' ...]'
-
-        # generate arg names
-        parts, i = [], 0
-        for i in range(min_count):
-            parts.append('%s%s' % (display_name, i + 1))
-        if len(parts) > 4:
-            parts = parts[:2] + ['...'] + parts[-1:]
-        if i < max_count:
-            max_parts = []
-            if (max_count - i) <= 2:
-                max_parts.extend(['%s%s' % (display_name, x + 1)
-                                  for x in range(i, max_count)])
-            else:
-                max_parts.append('...')
-                max_parts.append('%s%s' % (display_name, max_count))
-            max_part = '[%s]' % ' '.join(max_parts)
-            parts.append(max_part)
-
-        return ' '.join(parts)
-
+        return {'max_flag_doc_width': max_flag_doc_width,
+                'max_flag_width': max_flag_width,
+                'max_subcmd_width': max_subcmd_width,
+                'flag_doc_start': flag_doc_start,
+                'subcmd_doc_start': subcmd_doc_start,
+                'max_width': max_width}
 
 
 """Usage: cmd_name sub_cmd [..as many subcommands as the max] --flags args ...
@@ -226,156 +252,3 @@ def default_fmt_flag_label(self, flag):
 
 
 """
-
-import sys
-
-from boltons.iterutils import unique
-
-class HelpHandler(object):
-    default_context = {
-        'usage_label': 'Usage:',
-        'subcmd_section_heading': 'Subcommands: ',
-        'flags_section_heading': 'Flags: ',
-        'posargs_section_heading': 'Positional arguments:',
-        'section_break': '\n',
-        'group_break': '',
-        'subcmd_example': 'subcommand',
-        'min_doc_width': 40,
-        'doc_separator': ' ',
-        'section_indent': '  '
-    }
-
-    def __init__(self, flag=('--help', '-h'), func=None, **kwargs):
-        ctx = {}
-        for key, val in self.default_context.items():
-            ctx[key] = kwargs.pop(key, val)
-        if kwargs:
-            raise TypeError('unexpected keyword arguments: %r' % list(kwargs.keys()))
-        self.ctx = ctx
-        self.func = func if func is not None else self.default_help_func
-        if not callable(self.func):
-            raise TypeError('expected func to be callable, not %r' % func)
-
-    def default_help_func(self, cmd_, subcmds_):
-        print(self.get_help_text(cmd_.parser, subcmds=subcmds_))
-        sys.exit(0)
-
-    def get_help_text(self, parser, subcmds=(), flags=None):
-        # TODO: filter by actually-used flags (note that help_flag and
-        # flagfile_flag are semi-built-in, thus used by all subcommands)
-        ctx = self.ctx
-        widths = self.get_widths(parser, subcmds)
-
-        ret = [self.get_usage_line(parser, subcmds=subcmds)]
-        append = ret.append
-
-        if subcmds:
-            parser = parser.subprs_map[subcmds]
-
-        append(ctx['group_break'])
-
-        if parser.doc:
-            append(parser.doc)
-            append(ctx['section_break'])
-
-        if parser.subprs_map:
-            append(ctx['subcmd_section_heading'])
-            append(ctx['group_break'])
-            for sub_name in unique([sp[0] for sp in parser.subprs_map if sp]):
-                subprs = parser.subprs_map[(sub_name,)]
-                append(ctx['section_indent'] + sub_name + '   ' + subprs.doc)
-            append(ctx['section_break'])
-
-        flags = parser.path_flag_map[()]
-        shown_flags = [f for f in flags.values() if not f.display.hidden]
-        if shown_flags:
-            append(ctx['flags_section_heading'])
-            append(ctx['group_break'])
-            for flag in unique(shown_flags):
-                label = flag.display.label
-                label_f = label.ljust(widths['max_flag_width'])
-                lhs = ctx['section_indent'] + label_f + ctx['doc_separator']
-                doc_parts = [] if not flag.doc else [flag.doc]
-                doc_parts.append(flag.display.post_doc)
-                full_doc = ' '.join(doc_parts)
-                if not full_doc:
-                    append(lhs)
-                    continue
-
-                wrapped_doc = textwrap.wrap(full_doc, widths['max_flag_doc_width'])
-                first_line = lhs + wrapped_doc[0]
-                append(first_line)
-                if len(wrapped_doc) == 1:
-                    continue
-
-                for line in wrapped_doc[1:]:
-                    append(' ' * widths['flag_doc_start'] + line)
-
-                if flag.doc:
-                    pass # import pdb;pdb.set_trace()
-
-
-        return '\n'.join(ret)
-
-    def get_usage_line(self, parser, subcmds=()):
-        ctx = self.ctx
-        subcmds = tuple(subcmds or ())
-        parts = [ctx['usage_label']] if ctx['usage_label'] else []
-        append = parts.append
-
-        append(' '.join((parser.name,) + subcmds))
-
-        # TODO: put () in subprs_map to handle some of this sorta thing
-        if not subcmds and parser.subprs_map:
-            append('subcommand')
-        elif subcmds and parser.subprs_map[subcmds].subprs_map:
-            append('subcommand')
-
-        flags = parser.path_flag_map[subcmds]
-        shown_flags = [f for f in flags.values() if f.display_name is not False]
-
-        if shown_flags:
-            append('[FLAGS]')
-
-        if parser.posargs:
-            if parser.posargs.display_full:
-                append(parser.posargs.display_full)
-            elif parser.posargs.min_count:
-                append('args ...')
-            else:
-                append('[args ...]')
-
-        return ' '.join(parts)
-
-    def get_widths(self, prs, subprs_path=(), max_width=None):
-        if max_width is None:
-            try:
-                max_width = int(os.environ['COLUMNS'])
-            except (KeyError, ValueError):
-                max_width = 80
-            max_width -= 2
-
-        max_flag_width = 0
-        for flag in unique(prs.path_flag_map[subprs_path].values()):
-            cur_len = len(flag.display.label)
-            if cur_len > max_flag_width:
-                max_flag_width = cur_len
-
-        max_subcmd_width = 0
-        for subcmd_name in unique([path[0] for path in prs.subprs_map if path]):
-            cur_len = len(subcmd_name)
-            if cur_len > max_subcmd_width:
-                max_subcmd_width = cur_len
-
-        min_doc_width = self.ctx['min_doc_width']
-        max_flag_doc_width = max_width - max_flag_width - len(self.ctx['doc_separator'])
-
-        flag_doc_start = len(self.ctx['section_indent'] + self.ctx['doc_separator']) + max_flag_width
-        subcmd_doc_start = len(self.ctx['section_indent'] + self.ctx['doc_separator']) + max_subcmd_width
-
-        return {'max_flag_doc_width': max_flag_doc_width,
-                'max_flag_width': max_flag_width,
-                'max_subcmd_width': max_subcmd_width,
-                'flag_doc_start': flag_doc_start,
-                'subcmd_doc_start': subcmd_doc_start,
-                'max_width': max_width}

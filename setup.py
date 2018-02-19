6c034c23e383f1f5fd5f6de542efed94a605d20d
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-02-18 23:30:19 -0800

    adding inital setup.py mostly to get the name reserved, but also to celebrate a hundo commits

---
+++ setup.py
@@ -0,0 +1,54 @@
+"""A command-line interface parser and framework, friendly for users,
+full-featured for developers.
+"""
+
+from setuptools import setup
+
+
+__author__ = 'Mahmoud Hashemi'
+__version__ = '0.0.1'
+__contact__ = 'mahmoud@hatnote.com'
+__url__ = 'https://github.com/mahmoud/face'
+__license__ = 'BSD'
+
+
+setup(name='face',
+      version=__version__,
+      description="A command-line interface parser and framework, friendly for users, full-featured for developers.",
+      long_description=__doc__,
+      author=__author__,
+      author_email=__contact__,
+      url=__url__,
+      packages=['face', 'face.test'],
+      include_package_data=True,
+      zip_safe=False,
+      license=__license__,
+      platforms='any',
+      install_requires=['boltons'],
+      classifiers=[
+          'Topic :: Utilities',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Implementation :: PyPy', ]
+      )
+
+"""
+A brief checklist for release:
+
+* tox
+* git commit (if applicable)
+* Bump setup.py version off of -dev
+* git commit -a -m "bump version for x.y.z release"
+* python setup.py sdist bdist_wheel upload
+* bump docs/conf.py version
+* git commit
+* git tag -a vx.y.z -m "brief summary"
+* write CHANGELOG
+* git commit
+* bump setup.py version onto n+1 dev
+* git commit
+* git push
+
+"""

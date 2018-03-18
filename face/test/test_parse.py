57c1f996632ed953f3eab2846bec1553eecab58b
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-03-17 18:28:37 -0700

    foundational tox+pytest+coverage setup
    Add command description wrapping.

--- face/test/test_parse.py
+++ face/test/test_parse.py
@@ -1,13 +1,17 @@
 
-from face import Parser
+from face import Parser, ListParam
 
 
 def get_calc_parser():
     prs = Parser('cmd')
+
     sum_subprs = Parser('sum')
-    sum_subprs.add('--num', int, on_duplicate='extend')
+    sum_subprs.add('--num', multi=True, missing=0, parse_as=int,
+                   doc='a number to include in the sum, expects integers at the moment'
+                   ' because it is fun to change things later')
     prs.add(sum_subprs)
-    prs.add('--verbose', char='-V')
+
+    prs.add('--verbose', char='-V', parse_as=True)
     prs.add('--loop-count', parse_as=int)
 
     return prs
@@ -18,7 +22,7 @@ def test_calc_parser():
 
     res = prs.parse(['calc.py', '--verbose'])
 
-    assert res.name = 'calc.py'
+    assert res.name == 'calc.py'
     assert res.flags['verbose'] is True
 
     res = prs.parse(['calc.py', '--loop_count', '5'])
@@ -28,7 +32,7 @@ def test_calc_parser():
     assert res.flags['loop_count'] == 4
 
     res = prs.parse(['calc.py', 'sum', '--num', '4', '--num', '-2', '--num', '0'])
-    assert res.subcmds == ['sum']
+    assert res.subcmds == ('sum',)
     assert res.flags['num'] == [4, -2, 0]
 
     return

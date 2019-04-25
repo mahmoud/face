7cfe0647dfbcc85e6d93a4999903f453db2e46c9
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-04-25 09:51:28 -0700

    bumping version for 19.1.2dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '19.1.1'
+__version__ = '19.1.2dev'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/face'
 __license__ = 'BSD'

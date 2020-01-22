9fcb6cb990b1fa9fbe4158baaab49c8c39545de0
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-01-22 12:03:29 -0800

    bump version for v20.1.1 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '20.1.1dev'
+__version__ = '20.1.1'
 __contact__ = 'mahmoud@hatnote.com'
 __url__ = 'https://github.com/mahmoud/face'
 __license__ = 'BSD'

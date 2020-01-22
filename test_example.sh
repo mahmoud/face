a5396d90ed222d040d60a48d1c12c9a36878d654
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-01-21 16:02:30 -0800

    clean up some examples

--- test_example.sh
+++
@@ -1,5 +0,0 @@
-python example.py -V
-python example.py --loop-count 5 --verbose
-python example.py --no-such-flag
-python example.py sum --num 1 --num 2 --num 13 --num -1
-python example.py sum --num 2 --num 15 -- these dont matter

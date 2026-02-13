325fc5774cf0cad49205d8c01191dac2d7d23e81
Mahmoud Hashemi <mahmoud@hatnote.com>
2026-02-13 13:34:06 -0800

    fix rtd

--- docs/conf.py
+++ docs/conf.py
@@ -66,12 +66,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'sphinx_rtd_theme'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
     'navigation_depth': 4,
